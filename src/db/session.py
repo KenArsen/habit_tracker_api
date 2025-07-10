19aa1d2f65db20b42c9f806f7c3e0dffddd984f6
KenArsen <kenzhegulov.kgz@gmail.com>
2025-07-10 14:03:30 +0600

    Удален класс BaseModelMixin

--- src/db/session.py
+++ src/db/session.py
@@ -1,12 +1,23 @@
+from sqlalchemy import Column, DateTime, Integer
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+from sqlalchemy.sql import func
 
-from src.core.base_model import BaseModelMixin
 from src.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-class Base(BaseModelMixin, DeclarativeBase):
-    pass
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime(timezone=True), default=func.now())
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime(timezone=True), default=func.now(), onupdate=func.now())
